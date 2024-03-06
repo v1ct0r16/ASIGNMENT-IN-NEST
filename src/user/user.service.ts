f303da544831dedb5bb0583586dcfcd808b32bad
v1ct0r16 <pvictor1844@gmail.com>
2024-03-06 14:04:30 +0100

    just fixing my provider

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -17,12 +17,12 @@ export class UserService {
     //Add product
     async addProduct(payload){
         const add = this.UserRepository.create(payload);
-        return this.ProductRepository.save(add);
+        return this.UserRepository.save(add);
     }
 
     //get product by name
     async getOneByName(name :string): Promise<UserEntity> {
-        const find = await this.ProductRepository.findOne({ where: {name: name},});
+        const find = await this.UserRepository.findOne({ where: {name: name},});
 
     //error handling
     if (!find) {
@@ -34,7 +34,7 @@ export class UserService {
 
     // get one product by ID
     async getOneById(id :number): Promise<UserEntity> {
-        const find = await this.ProductRepository.findOne({ where: { id },});
+        const find = await this.UserRepository.findOne({ where: { id },});
     //error handling
     if (!find) {
         //this Error will be thrown if no such product id is not found in our database
@@ -45,7 +45,7 @@ export class UserService {
 
 //Delete by id
 async deleteById(id: number) {
-    const find = await this.ProductRepository.findOne({ where: { id },});
+    const find = await this.UserRepository.findOne({ where: { id },});
 //error handling
 if(!find) {
     //this Error will be thrown if no such product id is not found in our database
@@ -60,7 +60,7 @@ return {
 
 //Delete Product by name
 async deleteProductByName(name :string) :Promise<void> {
-    const find = await this.ProductRepository.delete({ name });
+    const find = await this.UserRepository.delete({ name });
     // if (result.affected === 0) {
     //     throw new HttpException('Product not found', 404); // it will be a  404 eroor, meaning not found
     // }
@@ -68,30 +68,30 @@ async deleteProductByName(name :string) :Promise<void> {
 
 // find all products 
 async findAll() {
-    return await this.ProductRepository.find()
+    return await this.UserRepository.find()
 }
 
 //Update product by name
 async updateProductByName(
     name: string,
-    payload: Partial<productEntity>,
-): Promise<productEntity> {
-    const product = await this.ProductRepository.findOne({ where: { name } });
-    if (!product) {
+    payload: Partial<UserEntity>,
+): Promise<UserEntity> {
+    const user = await this.ProductRepository.findOne({ where: { name } });
+    if (!user) {
         throw new HttpException('Product not found', 404); // it will be a  404 eroor, meaning not found
     }
 //Merge the payload into the existing product entity
-Object.assign(product, payload);
-return this.ProductRepository.save(product)
+Object.assign(user, payload);
+return this.UserRepository.save(user)
 } 
 
 async updateProduct(id, Payload) {
-    const update = this.ProductRepository.findOne({where: { id }});
+    const update = this.UserRepository.findOne({where: { id }});
     if (!update) {
         throw new  HttpException('Product not found', 404);
     }
 
-    const updateProduct = await this.ProductRepository.update(id, Payload);
+    const updateProduct = await this.UserRepository.update(id, Payload);
     return{
         statusCode:201,
         message: 'Product update succesfully',
